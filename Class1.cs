39d645be7b43c1e3c96649afaeaf8d9a7b2854a8
NMK007 <itsmenmk@gmail.com>
2017-06-17 12:31:06 +0530

    model

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+   
+    public class ApplianceStatus
+    {
+        public string id { get; set; }
+        public string value { get; set; }
+    }
+
+    public class SensorData
+    {
+        public string id { get; set; }
+        public string value { get; set; }
+
+        public string dateTime { get; set; }
+    }   
+}
